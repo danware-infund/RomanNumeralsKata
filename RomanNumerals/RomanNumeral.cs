ee966356b1bb7d3a377d914b394407953fb44453
Dan Ware <scandal.uk@gmail.com>
2019-07-09 23:05:26 +0100

    Refactored - use gradual transformations here

--- RomanNumerals/RomanNumeral.cs
+++ RomanNumerals/RomanNumeral.cs
@@ -2,17 +2,29 @@
 {
     public class RomanNumeral
     {
-        public static int NumeralToInteger(string numeral)
+        public static int NumeralToInteger(string value)
         {
-            switch (numeral)
+            var numericValue = 0;
+
+            if (!string.IsNullOrEmpty(value))
             {
-                case "I": return 1;
-                case "V": return 5;
-                case "X": return 10;
-                case "L": return 50;
-                case "C": return 100;
-                case "D": return 500;
-                case "M": return 1000;
+                numericValue = RomanCharacterValue(value[0]);
+            }
+
+            return numericValue;
+        }
+
+        private static int RomanCharacterValue(char value)
+        {
+            switch (value)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
                 default: return 0;
             }
         }
