8fa9e87b570994a3afbabb8571492232c6c91876
Dan Ware <scandal.uk@gmail.com>
2019-07-09 23:02:56 +0100

    Added all individual Roman characters

--- RomanNumerals/RomanNumeralUnitTests.cs
+++ RomanNumerals/RomanNumeralUnitTests.cs
@@ -8,29 +8,64 @@ namespace RomanNumerals
         [Fact]
         public void GivenNull_ShouldReturnZero()
         {
-            var output = RomanNumeral.NumeralToInteger(null);
-            output.Should().Be(0);
+            RomanNumeral.NumeralToInteger(null)
+                .Should().Be(0);
         }
 
         [Fact]
         public void GivenEmptyString_ShouldReturnZero()
         {
-            var output = RomanNumeral.NumeralToInteger(string.Empty);
-            output.Should().Be(0);
+            RomanNumeral.NumeralToInteger(string.Empty)
+                .Should().Be(0);
         }
 
         [Fact]
         public void GivenI_ShouldReturn1()
         {
-            var output = RomanNumeral.NumeralToInteger("I");
-            output.Should().Be(1);
+            RomanNumeral.NumeralToInteger("I")
+                .Should().Be(1);
         }
 
         [Fact]
         public void GivenV_ShouldReturn5()
         {
-            var output = RomanNumeral.NumeralToInteger("V");
-            output.Should().Be(5);
+            RomanNumeral.NumeralToInteger("V")
+                .Should().Be(5);
+        }
+
+        [Fact]
+        public void GivenX_ShouldReturn10()
+        {
+            RomanNumeral.NumeralToInteger("X")
+              .Should().Be(10);
+        }
+
+        [Fact]
+        public void GivenL_ShouldReturn50()
+        {
+            RomanNumeral.NumeralToInteger("L")
+              .Should().Be(50);
+        }
+
+        [Fact]
+        public void GivenC_ShouldReturn100()
+        {
+            RomanNumeral.NumeralToInteger("C")
+              .Should().Be(100);
+        }
+
+        [Fact]
+        public void GivenD_ShouldReturn500()
+        {
+            RomanNumeral.NumeralToInteger("D")
+              .Should().Be(500);
+        }
+
+        [Fact]
+        public void GivenM_ShouldReturn1000()
+        {
+            RomanNumeral.NumeralToInteger("M")
+              .Should().Be(1000);
         }
     }
 }
